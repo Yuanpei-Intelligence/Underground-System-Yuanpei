6aa046adeb91888224bc8602006eeeaeb26c8204
sunyb-dot <18211195741@126.com>
2021-07-16 15:04:59 +0800

    add change

--- Appointment/models.py
+++ Appointment/models.py
@@ -235,7 +235,7 @@ class Appoint(models.Model):
             data['Rtitle'] = '房间已删除'  # 房间名称
         return data
 
-
+from Appointment.utils.scheduler_func import cancel_scheduler
 @receiver(pre_delete, sender=Appoint)
 def before_delete_Appoint(sender, instance, **kwargs):
     cancel_scheduler(instance.Aid)
